a8a3e029ef187501ab258e066b0dcc4f0ab9bf23
heritio <herri_93@hotmail.com>
2021-09-18 02:50:39 +0200

    finished implementing carousel

--- index.js
+++ index.js
@@ -8,20 +8,32 @@ let nextBtn = document.querySelector("#next");
 let imgIndex = 1;
 let size = carouselItems[0].clientWidth;
 
-innerCarousel.style.transform = `translateX(${+(-size * imgIndex)}px)`;
+innerCarousel.style.transform = `translateX(${+(-size * imgIndex)}px)`; //starting position
 
 nextBtn.addEventListener("click", () => {
-  innerCarousel.style.transition = "transform 0.4s ease";
+  if (imgIndex >= carouselItems.length - 3) return; //stop and wait for transition to finish before starting another slide round
+  innerCarousel.style.transition = "transform 0.8s ease";
   imgIndex++;
   innerCarousel.style.transform = `translateX(${+(-size * imgIndex)}px)`;
 });
 
 prevBtn.addEventListener("click", () => {
-  innerCarousel.style.transition = "transform 0.4s ease";
+  if (imgIndex <= 0) return; //stop and wait for transition to finish before starting another slide round
+  innerCarousel.style.transition = "transform 0.8s ease";
   imgIndex--;
   innerCarousel.style.transform = `translateX(${+(-size * imgIndex)}px)`;
 });
 
+//check if last or first item and start another round or of same carousel items
 innerCarousel.addEventListener("transitionend", () => {
-  console.log("Transition");
+  if (carouselItems[imgIndex].id === "last-clone") {
+    innerCarousel.style.transition = "none";
+    imgIndex = carouselItems.length - 3;
+    innerCarousel.style.transform = `translateX(${+(-size * imgIndex)}px)`;
+  }
+  if (carouselItems[imgIndex].id === "first-clone") {
+    innerCarousel.style.transition = "none";
+    imgIndex = carouselItems.length - imgIndex;
+    innerCarousel.style.transform = `translateX(${+(-size * imgIndex)}px)`;
+  }
 });
